353353aefe191472115272aae6c418f0c6264793
s2310176 <216670039+s2310176@users.noreply.github.com>
2025-06-23 08:23:39 +0000

    Apply Code Formatter

--- Hit_and_Blow.py
+++ Hit_and_Blow.py
@@ -61,9 +61,13 @@ class HitAndBlowGame:
         first_digit = int(document.getElementById("first-digit").value)
         second_digit = int(document.getElementById("second-digit").value)
         third_digit = int(document.getElementById("third-digit").value)
-        
+
         # 3つの数字のうち2つ以上同じ数字なら警告を出し、再入力を求めるように修正
-        if ((first_digit == second_digit) or (second_digit == third_digit) or (third_digit == first_digit)) :
+        if (
+            (first_digit == second_digit)
+            or (second_digit == third_digit)
+            or (third_digit == first_digit)
+        ):
             alert("3桁はすべて異なる数字を入力してください。")
             return
         player_input = first_digit * 100 + second_digit * 10 + third_digit
